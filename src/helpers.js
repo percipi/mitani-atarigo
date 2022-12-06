09f82d1476895172dddd0500b282fa95634fe53a
Lukasz Pater <lukaszjpater@gmail.com>
2022-12-06 09:24:20 +0100

    some additional fixes;

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,5 @@
+ // @ts-check
+
 import {COLOR} from './consts.js';
 
 function getOppColor(color) {
@@ -19,13 +21,70 @@ function isMokuInArray(el, arr) {
   return false;
 }
 
+function Group(stones, liberties) {
+  this.stones = stones;
+  this.liberties = liberties;
+}
+
+function ownStonesNextTo(x,y,color) {
+  var moku,n, r=[];
+  moku = this.board[y][x];
+  n = moku.neighbors;
+  for (var i = 0; i< n.length; i++){
+      if (color === n[i].val){
+          r.push(n[i]);
+      }
+  }
+  return r;
+}
+
+function getNoGroupLiberties(group) {
+  let lib, res = [];
+  for (var i = 0; i < group.stones.length; i++) {
+      lib = hasEmptyNeighbour(group.stones[i]);
+      for (var j = 0; j < lib.length; j++) {
+          if (!isMokuInArray(lib[j], res)) {
+              //TODO bardzo nieladnie ale chce sprawdzic czy dziala
+              //zamiast tego stworzyc obiekt boardAnalyzer
+              if (!isMokuInArray(lib[j], group.stones)) {
+                  res.push(lib[j]);
+              }
+
+          }
+      }
+  }
+  return res.length;
+}
+
+function getGroup(moku) {
+  ////console.log("getGroup...");
+  var _alreadyChecked = [],
+      addNeighbours = function (moku, alreadyChecked) {
+          ////console.log("addNeighbours... for stone:", moku);
+          alreadyChecked.push(moku);
+          var res = [moku];
+          var n = ownStonesNextTo(moku.x,moku.y, moku.val);
+          ////console.log("addNeighbours... ownStonesNextTo:", n);
+          for (var i = 0; i < n.length; i++) {
+              if (!isMokuInArray(n[i], alreadyChecked)) {
+                  res = res.concat(addNeighbours(n[i], alreadyChecked));
+              }
+          }
+          ////console.log("addNeighbours... result:", res);
+          
+          return res;
+      };
+  var group = addNeighbours(moku, _alreadyChecked);
+  return new Group(group, getNoGroupLiberties({stones:group}));
+}
+
 //returns array of groups which contains stones passed as argument
 function getGroupsForStones(stones) {  
   var _alreadyInGroup = [];
   var groups = [];
   for (var i = 0; i < stones.length; i++) {
       if (!isMokuInArray(stones[i], _alreadyInGroup)) {
-          var group = this.getGroup(stones[i]);
+          var group = getGroup(stones[i]);
           //create reference from stone to his group
           for (var j=0; j<group.stones.length; j++) {
               group.stones[j].group = group;    
@@ -37,9 +96,9 @@ function getGroupsForStones(stones) {
   return groups;
 }
 
-function getCapturedGroups([x, y], color, intersections) {
-  var oppColor = getOppColor(color),
-  moku = intersections[getIntersectionIndex([x,y])];
+function getCapturedGroups(coord, color, intersections) {
+  var oppColor = getOppColor(color);
+    //moku = intersections[getIntersectionIndex([x,y])];
   let groups = getGroupsForStones(intersections);
 
   if (groups[oppColor].length === 0) {
@@ -47,11 +106,11 @@ function getCapturedGroups([x, y], color, intersections) {
   }
   else {
       var capturedGroups = [];
-      //kazde empty pole musi miec liste grup sasiadujacych
-      for (var i=0; i<moku.neighbors.length; i++){
-          if (moku.neighbors[i].notEmpty() && moku.neighbors[i].val === oppColor 
-              && moku.neighbors[i].group.liberties === 1) {
-              capturedGroups.push(moku.neighbors[i].group);
+      
+      var neighbours = getNeighbours(coord, intersections);
+      for (var i=0; i<neighbours.length; i++){
+          if (neighbours[i].color === oppColor && neighbours[i].group.liberties === 1) {
+              capturedGroups.push(neighbours[i].group);
           }
       }
   return capturedGroups;
@@ -96,7 +155,7 @@ function getNeighbours(moveCoord, intersections) {
   return neig;
 }
 
-function emptyNextTo(moveCoord, intersections) {
+function hasEmptyNeighbour(moveCoord, intersections) {
   var neighbours = getNeighbours(moveCoord, intersections),
       res = [];
   ////console.log("Neighbours length: ", neighbours.length);
@@ -111,7 +170,7 @@ function emptyNextTo(moveCoord, intersections) {
 }
 
 function isSuicide(moveCoord, color, intersections) {
-  if ((emptyNextTo(moveCoord, intersections).length !== 0) || getCapturedGroups(moveCoord, color).length !== 0) {
+  if ((hasEmptyNeighbour(moveCoord, intersections).length !== 0) || getCapturedGroups(moveCoord, color, intersections).length !== 0) {
       return false;
   }
   else {
